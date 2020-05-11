cdf856f417b6a384f861a614d51a144d0d1e2e37
Gordon Macintyre <gordon.c.macintyre@gmail.com>
2020-05-11 16:41:56 +0100

    Quiz details updates after editing

--- lib/screens/library/quizzes/quiz_details_page.dart
+++ lib/screens/library/quizzes/quiz_details_page.dart
@@ -3,7 +3,7 @@ import 'package:qmhb/models/quiz_model.dart';
 import 'package:qmhb/screens/library/quizzes/quiz_editor_page.dart';
 import 'package:qmhb/shared/widgets/quiz_details_widget.dart';
 
-class QuizDetailsPage extends StatelessWidget {
+class QuizDetailsPage extends StatefulWidget {
   final QuizModel quizModel;
 
   QuizDetailsPage({
@@ -11,16 +11,28 @@ class QuizDetailsPage extends StatelessWidget {
   });
 
   @override
+  _QuizDetailsPageState createState() => _QuizDetailsPageState();
+}
+
+class _QuizDetailsPageState extends State<QuizDetailsPage> {
+  QuizModel quizModel;
+
+  @override
+  void initState() {
+    super.initState();
+    quizModel = widget.quizModel;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Quiz Details"),
         actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.edit),
-            label: Text('Edit'),
-            onPressed: () {
-              Navigator.of(context).push(
+          FlatButton(
+            child: Text('Edit'),
+            onPressed: () async {
+              final quiz = await Navigator.of(context).push(
                 MaterialPageRoute(
                   builder: (context) => QuizEditorPage(
                     type: QuizEditorPageType.EDIT,
@@ -28,12 +40,15 @@ class QuizDetailsPage extends StatelessWidget {
                   ),
                 ),
               );
+              setState(() {
+                quizModel = quiz;
+              });
             },
           ),
         ],
       ),
       body: QuizDetailsWidget(
-        quizModel: quizModel,
+        quizModel: widget.quizModel,
       ),
     );
   }
